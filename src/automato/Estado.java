c4f3de2fa5f7956661c0d6628cd83c19eeecd68d
lsamoyedem <lucassamoyedem@gmail.com>
2022-11-24 01:11:16 -0300

    Primeiro Commit
    Primeiro Commit

---
+++ src/automato/Estado.java
@@ -0,0 +1,85 @@
+package automato;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Lucas Samoyedem
+ */
+public class Estado {
+
+    private int numero;
+    private char letra;
+    private boolean estadoInical;
+    private boolean estadoFinal;
+    private List<Estado> proximosEstados = new ArrayList<>();
+
+    public Estado(int numero, char letra) {
+        this.numero = numero;
+        this.letra = letra;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public char getLetra() {
+        return letra;
+    }
+
+    public void setLetra(char letra) {
+        this.letra = letra;
+    }
+
+    public List<Estado> getProximosEstados() {
+        return proximosEstados;
+    }
+
+    public void setProximosEstados(List<Estado> proximosEstados) {
+        this.proximosEstados = proximosEstados;
+    }
+
+    public boolean isEstadoInical() {
+        return estadoInical;
+    }
+
+    public void setEstadoInical(boolean estadoInical) {
+        this.estadoInical = estadoInical;
+    }
+
+    public boolean isEstadoFinal() {
+        return estadoFinal;
+    }
+
+    public void setEstadoFinal(boolean estadoFinal) {
+        this.estadoFinal = estadoFinal;
+    }
+
+    public String getDescricaoEstado() {
+        String q = "";
+        if (estadoInical) {
+            q += "->";
+        }
+        if (estadoFinal) {
+            q += "*";
+        }
+        q += "Q" + numero;
+        return q;
+    }
+
+    public Estado getProximoEstado(char token) {
+        if (proximosEstados != null && !proximosEstados.isEmpty()) {
+            for (Estado proximosEstado : proximosEstados) {
+                if (proximosEstado.getLetra() == token) {
+                    return proximosEstado;
+                }
+            }
+        }
+        return null;
+    }
+}
---
+++ src/automato/Main.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package automato;
+
+import com.formdev.flatlaf.FlatDarkLaf;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+/**
+ *
+ * @author Lucas Samoyedem
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(new FlatDarkLaf());
+        } catch (UnsupportedLookAndFeelException ex) {
+            ex.printStackTrace();
+        }
+        new App().setVisible(true);
+    }
+}
